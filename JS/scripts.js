0b234fbe1c2bfc3040ff8c107fa01c0d00c7a6be
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-10-09 16:24:00 -0300

    Iniciando o cálculo do IMC

--- JS/scripts.js
+++ JS/scripts.js
@@ -38,63 +38,50 @@ const data = [
   ];
   
   // Seleção de elementos
+ 
   const imcTable = document.querySelector("#imc-table");
-  
+
+
   const heightInput = document.querySelector("#height");
   const weightInput = document.querySelector("#weight");
   const calcBtn = document.querySelector("#calc-btn");
-  const clearBtn = document.querySelector("#clear-btn");
-  
-  const calcContainer = document.querySelector("#calc-container");
-  const resultContainer = document.querySelector("#result-container");
-  
-  const imcNumber = document.querySelector("#imc-number span");
-  const imcInfo = document.querySelector("#imc-info span");
-  
-  const backBtn = document.querySelector("#back-btn");
+  const clearBtn = document.querySelector("#clean-btn");
   
   // Funções
   function createTable(data) {
     data.forEach((item) => {
+
       const div = document.createElement("div");
       div.classList.add("table-data");
-  
+
       const classification = document.createElement("p");
       classification.innerText = item.classification;
-  
+
+
       const info = document.createElement("p");
       info.innerText = item.info;
-  
+
+
       const obesity = document.createElement("p");
       obesity.innerText = item.obesity;
-  
+
       div.appendChild(classification);
       div.appendChild(info);
       div.appendChild(obesity);
-  
+
+
       imcTable.appendChild(div);
+
     });
   }
-  
-  function validDigits(text) {
-    return text.replace(/[^0-9,]/g, "");
-  }
-  
-  function calcImc(height, weight) {
-    const imc = (weight / (height * height)).toFixed(1);
-    return imc;
-  }
-  
+
   function cleanInputs() {
     heightInput.value = "";
     weightInput.value = "";
-    imcNumber.classList = "";
-    imcInfo.classList = "";
   }
-  
-  function showOrHideResults() {
-    calcContainer.classList.toggle("hide");
-    resultContainer.classList.toggle("hide");
+
+  function validDigits(text) {
+    return text.replace(/[^0-9,]/g, "");
   }
   
   // Init
@@ -102,71 +89,28 @@ const data = [
   
   // Eventos
   [heightInput, weightInput].forEach((el) => {
-    el.addEventListener("input", (e) => {
-      const updatedValue = validDigits(e.target.value);
-  
-      e.target.value = updatedValue;
-    });
+  el.addEventListener("input", (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    e.target.value = updatedValue;
   });
-  
+});
+
   calcBtn.addEventListener("click", (e) => {
     e.preventDefault();
-  
-    const weight = +weightInput.value.replace(",", ".");
-    const height = +heightInput.value.replace(",", ".");
-  
-    console.log(weight, height);
-  
-    if (!weight || !height) return;
-  
-    const imc = calcImc(height, weight);
-    let info;
-  
-    data.forEach((item) => {
-      if (imc >= item.min && imc <= item.max) {
-        info = item.info;
-      }
-    });
-  
-    if (!info) return;
-  
-    imcNumber.innerText = imc;
-    imcInfo.innerText = info;
 
-  
-    switch (info) {
-      case "Magreza":
-        imcNumber.classList.add("low");
-        imcInfo.classList.add("low");
-        break;
-      case "Normal":
-        imcNumber.classList.add("good");
-        imcInfo.classList.add("good");
-        break;
-      case "Sobrepeso":
-        imcNumber.classList.add("low");
-        imcInfo.classList.add("low");
-        break;
-      case "Obesidade":
-        imcNumber.classList.add("medium");
-        imcInfo.classList.add("medium");
-        break;
-      case "Obesidade grave":
-        imcNumber.classList.add("high");
-        imcInfo.classList.add("high");
-        break;
-    }
-  
-    showOrHideResults();
+    const weight = +weightInput.value.replace(",",".");
+    const height = +heightInput.value.replace(",",".");
+
+    console.log(weight, height);
+
   });
-  
+
+
   clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
-  
+
+
     cleanInputs();
   });
   
-  backBtn.addEventListener("click", (e) => {
-    cleanInputs();
-    showOrHideResults();
-  }); 
